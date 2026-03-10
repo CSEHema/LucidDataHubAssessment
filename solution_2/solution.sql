366b2df1f4b52dd7f0758319ab8926f955ab8f2f
Mandalemula Hema Siri Ramya <hemasiri1405@gmail.com>
2026-03-10 15:39:35 +0530

    Add SQL query to retrieve top 10 word counts

--- solution_2/solution.sql
+++ solution_2/solution.sql
@@ -1,1 +1,17 @@
-
+WITH word_counts AS (
+    SELECT
+        word,
+        COUNT(*) AS cnt
+    FROM words_table
+    GROUP BY word
+),
+ranked AS (
+    SELECT
+        word,
+        cnt,
+        DENSE_RANK() OVER (ORDER BY cnt DESC) AS rnk
+    FROM word_counts
+)
+SELECT word, cnt
+FROM ranked
+WHERE rnk <= 10;
